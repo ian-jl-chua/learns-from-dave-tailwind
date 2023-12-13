bb09ba8a2b0f892b9b37762b25474b06205b8882
Ian Chua <ianchuajl@hotmail.com>
2023-12-14 12:41:52 +1300

    final push and finised TailwindCss course

--- tailwind02/tailwind.config.js
+++ tailwind02/tailwind.config.js
@@ -1,23 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.html', './build/js/*.js'],
+  content: ["./build/**/*.{html,js}"],
+  // content: ["./build/*.html", "./build/js/*.js"],
+  // doing this ^ is explicitly telling tailwind where to look
   theme: {
     extend: {
+      colors: {
+        papayawhip: {
+          light: "#fef4e4",
+          DEFAULT: "#ffefd5",
+          dark: "#fee5bc",
+        },
+      },
+      // here's an example on how to extend colors
       screens: {
-        widescreen: { raw: '(min-aspect-ratio: 3/2)' },
-        tallscreen: { raw: '(min-aspect-ratio: 13/20)' },
+        widescreen: { raw: "(min-aspect-ratio: 3/2)" },
+        tallscreen: { raw: "(max-aspect-ratio: 13/20)" },
       },
       keyframes: {
-        'open-menu': {
-          '0%': { transform: 'scaleY(0)' },
-          '80%': { transform: 'scaleY(1.1)' },
-          '100%': { transform: 'scaleY(1)' },
+        "open-menu": {
+          "0%": { transform: "scaleY(0)" },
+          "80%": { transform: "scaleY(1.1)" },
+          "100%": { transform: "scaleY(1)" },
         },
       },
       animation: {
-        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+        "open-menu": "open-menu 0.5s ease-in-out forwards",
       },
     },
   },
   plugins: [],
-}
+};
